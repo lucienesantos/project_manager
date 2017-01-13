a30d52b4d4047ab07da39829f4de271b1ab5825c
Luciene Santos <lucienesantosmc@gmail.com>
2017-01-13 13:02:12 -0300

    Add endpoint create project

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -4,5 +4,24 @@ class Api::V1::ProjectsController < ApplicationController
     projects = Project.all.sort_by_creation
     render json: projects
   end
+
+  def create
+
+    project = Project.new(project_params)
+    project.client = Client.find_by_id(project_params[:client_id])
+
+    if project.save
+      render json: { project: project }, status: :created
+    else
+      render json: { errors: project.errors.full_messages }, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:name, :conclusion_at, :client_id)
+    end
   
 end
--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -5,30 +5,82 @@ RSpec.describe Api::V1::ProjectsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(200)
+      expect(response.status).to eq(200)
+    end
+
+    context "When return content body" do
+
+      let!(:bradesco_project){ create(:project, name: "Bradesco's Project") }
+      let!(:itau_project) { create(:project, name: "Itau's Project") }
+      let!(:santander_project) { create(:project, name: "Santander's Project") }
+
+      let(:body) { JSON.parse(response.body) }
+
+      it "returns projects count" do
+        get :index
+        expect(body.count).to eq(3)
+      end
+
+      it "returns project ordened by date of created decrescent" do
+        get :index
+        expect(body[0]["name"]).to eq("Santander's Project")
+        expect(body[1]["name"]).to eq("Itau's Project")
+        expect(body[2]["name"]).to eq("Bradesco's Project")
+      end
     end
   end
 
-  context "When return content body" do
+  describe "POST #create" do
+    let(:client){ create(:client) }
 
-    let!(:bradesco_project){ create(:project, name: "Bradesco's Project") }
-    let!(:itau_project) { create(:project, name: "Itau's Project") }
-    let!(:santander_project) { create(:project, name: "Santander's Project") }
+    context "with valid attributes" do
 
-    let(:body) { JSON.parse(response.body) }
+      let(:project_params) do
+        {
+          project: {
+            name: "Bradesco's Project", conclusion_at: "2017-10-22 08:00:00", client_id: client.id,
+          }
+        }
+      end
 
-    it "returns projects count" do
-      get :index 
-      expect(body.count).to eq(3)
+      it "request whit success return status created" do
+        post :create, params: project_params
+        expect(response.status).to eq(201)
+        expect(response).to have_http_status(:created)
+      end
+
+      it "created a project" do
+        expect{post :create, params: project_params}.to change{Project.count }.by(1)
+      end
+
+      it "returns project created" do
+        post :create, params: project_params
+        body = JSON.parse(response.body)
+
+        expect(body["project"]["name"]).to eq("Bradesco's Project")
+      end
+
     end
 
-    it "returns project ordened by date of created decrescent" do
-      get :index
-      expect(body[0]["name"]).to eq("Santander's Project")
-      expect(body[1]["name"]).to eq("Itau's Project")
-      expect(body[2]["name"]).to eq("Bradesco's Project")
-    end
+    context "whit invalids attributes" do
+      let(:project_params) do
+        {
+          project: {
+            name: "Bradesco's Project", conclusion_at: "1", client_id: client.id,
+          }
+        }
+      end
 
-  end  
+      it "does not should create project" do
+        expect {post :create, params: project_params}.to_not change{Project.count}
+      end
+
+      it "request return error code" do
+        post :create, params: project_params
+        expect(response.status).to eq(422)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 
 end
